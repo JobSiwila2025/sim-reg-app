3d2525917f4c01c9625d4bcf725543a72a839de2
Your Name <your@email.com>
2026-04-04 00:24:49 +0200

    Update contract address after redeployment

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import simContractABI from './SimContractABI.json';
 import './App.css';
 
 // Replace this with your deployed smart contract address
-const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const contractAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
 
 function App() {
   const [walletAddress, setWalletAddress] = useState('');
